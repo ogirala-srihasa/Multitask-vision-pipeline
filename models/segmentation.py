0da1ea00a3705a519a9fbbb17259e4306a23ed22
InfiniteOS6 <infiniteos456@gmail.com>
2026-04-08 16:58:16 +0530

    multitask done

--- models/segmentation.py
+++ models/segmentation.py
@@ -3,12 +3,29 @@
 
 import torch
 import torch.nn as nn
-
+from .vgg11 import VGG11
+from .layers import CustomDropout
 class VGG11UNet(nn.Module):
     """U-Net style segmentation network.
     """
+    def _make_block(self, in_ch, out_ch, batch_norm,dropout_p,num_conv):
+        layers = []
+        layers.append(nn.Conv2d(in_ch, out_ch, 3, padding=1))
+        if batch_norm:
+            layers.append(nn.BatchNorm2d(out_ch))
+        layers.append(nn.ReLU(inplace=True))
+        layers.append(CustomDropout(dropout_p))
+        if(num_conv > 1):
+            layers.append(nn.Conv2d(out_ch,out_ch,kernel_size= 3, padding= 1))
+            if batch_norm:
+                layers.append(nn.BatchNorm2d(out_ch))
+            layers.append(nn.ReLU(inplace=True))
+            layers.append(CustomDropout(dropout_p))
 
-    def __init__(self, num_classes: int = 3, in_channels: int = 3, dropout_p: float = 0.5):
+        return nn.Sequential(*layers)
+    
+
+    def __init__(self, num_classes: int = 3, in_channels: int = 3, dropout_p: float = 0.5, batch_norm : bool = True):
         """
         Initialize the VGG11UNet model.
 
@@ -17,7 +34,24 @@ class VGG11UNet(nn.Module):
             in_channels: Number of input channels.
             dropout_p: Dropout probability for the segmentation head.
         """
-        pass
+        super().__init__()
+        # Encoder
+        self.VGGhead = VGG11(in_channels= in_channels,batch_norm= batch_norm)
+
+        # Decoder 
+        self.up1 = nn.ConvTranspose2d(512, 512, kernel_size=2, stride=2)
+        self.dec1 = self._make_block(1024,512,batch_norm=batch_norm,dropout_p=dropout_p,num_conv= 2)
+        self.up2 = nn.ConvTranspose2d(512, 512, kernel_size=2, stride=2)
+        self.dec2 = self._make_block(1024,256,batch_norm,dropout_p,2)
+        self.up3 = nn.ConvTranspose2d(256, 256, kernel_size=2, stride=2)
+        self.dec3 = self._make_block(512,128,batch_norm,dropout_p,2)
+        self.up4 = nn.ConvTranspose2d(128, 128, kernel_size=2, stride=2)
+        self.dec4 = self._make_block(256,64,batch_norm,dropout_p,1)
+        self.up5 = nn.ConvTranspose2d(64, 64, kernel_size=2, stride=2)
+        self.dec5 = self._make_block(128,32,batch_norm,dropout_p,1)
+        self.outConv = nn.Conv2d(32, num_classes, kernel_size=1)
+
+
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Forward pass for segmentation model.
@@ -27,5 +61,28 @@ class VGG11UNet(nn.Module):
         Returns:
             Segmentation logits [B, num_classes, H, W].
         """
-        # TODO: Implement forward pass.
-        raise NotImplementedError("Implement VGG11UNet.forward")
+        bottleneck, feature_maps = self.VGGhead(x, return_features=True)
+
+        u1 = self.up1(bottleneck)
+        cat1 = torch.cat([u1, feature_maps['block5']], dim=1)
+        x1 = self.dec1(cat1)
+
+        u2 = self.up2(x1)
+        cat2 = torch.cat([u2, feature_maps['block4']], dim=1)
+        x2 = self.dec2(cat2)
+
+        u3 = self.up3(x2)
+        cat3 = torch.cat([u3, feature_maps['block3']], dim=1)
+        x3 = self.dec3(cat3)
+
+        u4 = self.up4(x3)
+        cat4 = torch.cat([u4, feature_maps['block2']], dim=1)
+        x4 = self.dec4(cat4)
+
+        u5 = self.up5(x4)
+        cat5 = torch.cat([u5, feature_maps['block1']], dim=1)
+        x5 = self.dec5(cat5)
+
+        out = self.outConv(x5)
+
+        return out
--- train.py
+++ train.py
@@ -13,15 +13,75 @@ from sklearn.metrics import f1_score
 from data.pets_dataset import OxfordIIITPetDataset
 from models.classification import VGG11Classifier
 from models.localization import VGG11Localizer
+from models.segmentation import VGG11UNet
 from losses.iou_loss import IoULoss
 import argparse
+
+def dice_loss(pred_logits, target, num_classes=3, eps=1e-6):
+    """
+    pred_logits: [B, num_classes, H, W] raw logits
+    target:      [B, H, W] integer class indices {0,1,2}
+    returns:     scalar loss value in [0, 1]
+    """
+    B, C, H, W = pred_logits.shape
+    
+    # converting logits to probabilities
+    pred_prob = torch.softmax(pred_logits,dim = 1)
+    
+    # one-hot encoding target to [B, C, H, W]
+    one_hot = torch.zeros(B, num_classes, H, W, device=target.device)
+    one_hot.scatter_(1, target.unsqueeze(1), 1)
+    
+    # computing per-class Dice score
+    # flatten spatial dims for easier computation
+    probs = pred_prob.view(B, C, -1)
+    one_hot = one_hot.view(B, C, -1)
+
+    #computing intersection and sums per class
+    intersection = torch.sum(probs * one_hot, dim=2)
+    pred_sum = torch.sum(probs, dim=2)
+    target_sum = torch.sum(one_hot, dim=2)
+
+    dice_score = (2. * intersection + eps) / (pred_sum + target_sum + eps)
+    
+    # Dice loss = 1 - mean(Dice scores across classes and batch)
+    return 1 - dice_score.mean()
+
+def dice_score(pred_logits, target, num_classes=3, eps=1e-6):
+    """
+    pred_logits: [B, num_classes, H, W] raw logits
+    target:      [B, H, W] integer class indices {0,1,2}
+    returns:     scalar score value
+    """
+    B, C, H, W = pred_logits.shape
+    
+    # converting logits to probabilities
+    pred_prob = torch.softmax(pred_logits,dim = 1)
+    
+    # one-hot encoding target to [B, C, H, W]
+    one_hot = torch.zeros(B, num_classes, H, W, device=target.device)
+    one_hot.scatter_(1, target.unsqueeze(1), 1)
+    
+    # computing per-class Dice score
+    # flatten spatial dims for easier computation
+    probs = pred_prob.view(B, C, -1)
+    one_hot = one_hot.view(B, C, -1)
+
+    #computing intersection and sums per class
+    intersection = torch.sum(probs * one_hot, dim=2)
+    pred_sum = torch.sum(probs, dim=2)
+    target_sum = torch.sum(one_hot, dim=2)
+
+    dice_score = (2. * intersection + eps) / (pred_sum + target_sum + eps)
+    
+    return dice_score.mean()
 
 def parse_arguments():
     """
     Parse command-line arguments.
     """
     parser = argparse.ArgumentParser(description='Train a neural network')
-    parser.add_argument('-t','--task',type = str, default='classification', choices=['classification','localization'])
+    parser.add_argument('-t','--task',type = str, default='classification', choices=['classification','localization','segmentation'])
     parser.add_argument('-e', '--epochs', type=int, default=10, help='Number of epochs')
     parser.add_argument('-b', '--batch_size', type = int ,default= 128, help='Mini-batch size')
     parser.add_argument('-lr', '--learning_rate', type= float, default= 0.001, help= 'Learning rate for optimizer')
@@ -135,6 +195,57 @@ def validate_localization(model, loader,mse_loss,mean_iou_loss,sample_iou_loss,d
     avg_loss = total_loss / len(loader)
     avg_iou  = total_iou / len(loader)
     return avg_loss, avg_iou
+
+def train_one_epoch_segmentation(model, loader, ce_loss, optimizer, device):
+    model.train()
+    total_loss = 0.0
+    total_dice_score = 0.0
+
+    for images, labels, bboxes, masks in loader:
+        images = images.to(device)
+        masks = masks.to(device)      
+
+        optimizer.zero_grad()
+        pred_mask = model(images)      
+
+        # combined loss
+        loss_ce = ce_loss(pred_mask, masks)
+        loss_dice= dice_loss(pred_mask, masks)
+        loss = loss_ce + loss_dice
+
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+        with torch.no_grad():
+            total_dice_score += dice_score(pred_mask,masks).item()
+
+    avg_loss = total_loss / len(loader)
+    avg_dice_score  = total_dice_score/ len(loader)
+    return avg_loss, avg_dice_score
+
+def validate_segmentation(model, loader,ce_loss,device):
+    model.eval()
+    total_loss = 0.0
+    total_dice_score = 0.0 
+    with torch.no_grad():
+        for images, labels, bboxes, masks in loader:
+            images = images.to(device)
+            masks = masks.to(device)      
+
+            pred_mask = model(images)       
+
+            # combined loss
+            loss_mse = ce_loss(pred_mask, masks)
+            loss_iou = dice_loss(pred_mask, masks)
+            loss = loss_mse + loss_iou
+
+            total_loss += loss.item()
+            total_dice_score += dice_score(pred_mask,masks).item()
+
+    avg_loss = total_loss / len(loader)
+    avg_dice_score  = total_dice_score / len(loader)
+    return avg_loss, avg_dice_score
 
 def main():
     #collecting CLI args
@@ -271,7 +382,64 @@ def main():
                 "best_val/iou": best_val_iou
             })
     else:
-        return 
+        model = VGG11UNet(dropout_p=args.dropout, batch_norm=batch_norm)
+        ce_loss = nn.CrossEntropyLoss()
+
+        # transfer learning
+        if os.path.exists("checkpoints/classifier.pth"):
+            checkpoint = torch.load("checkpoints/classifier.pth", map_location=device)
+            full_state = checkpoint.get("state_dict", checkpoint)
+            encoder_state = {
+                k.replace("VGGhead.", ""): v
+                for k, v in full_state.items()
+                if k.startswith("VGGhead.")
+            }
+            model.VGGhead.load_state_dict(encoder_state)
+            print("Loaded encoder weights from classifier.pth ")
+            model.to(device=device)
+            decoder_params = list(model.up1.parameters()) + list(model.dec1.parameters()) + \
+                 list(model.up2.parameters()) + list(model.dec2.parameters()) + \
+                 list(model.up3.parameters()) + list(model.dec3.parameters()) + \
+                 list(model.up4.parameters()) + list(model.dec4.parameters()) + \
+                 list(model.up5.parameters()) + list(model.dec5.parameters()) + \
+                 list(model.outConv.parameters()
+            )
+
+            optimizer = optim.Adam([
+                {"params": model.VGGhead.parameters(), "lr": args.learning_rate * 0.1},
+                {"params": decoder_params, "lr": args.learning_rate},
+            ])
+        else:
+            print("No classifier checkpoint found, training from scratch")
+            model.to(device=device)
+            optimizer = optim.Adam(model.parameters(), lr = args.learning_rate)
+        best_val_dice_score = 0
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer, mode='max', patience=3, factor=0.5
+        )
+        for epoch in range(args.epochs):
+            train_loss, train_dice_score = train_one_epoch_segmentation(model, train_loader,ce_loss= ce_loss, optimizer= optimizer,device= device)
+            val_loss, val_dice_score    = validate_segmentation(model, val_loader,ce_loss=ce_loss,device= device)
+            scheduler.step(val_dice_score)
+            print(f"Epoch {epoch+1}: train_loss={train_loss:.4f}, val_dice_score={val_dice_score:.4f}")
+
+            if val_dice_score > best_val_dice_score:
+                best_val_dice_score = val_dice_score
+                torch.save({
+                    "state_dict": model.state_dict(),
+                    "epoch": epoch,
+                    "best_metric": best_val_dice_score,
+                }, "checkpoints/unet.pth")
+                print(f"Saved best model (val_dice_score={best_val_dice_score:.4f})")
+
+            wandb.log({
+                "epoch": epoch,
+                "train/loss": train_loss,
+                "train/dice_score": train_dice_score,
+                "val/loss": val_loss,
+                "val/dice_score": val_dice_score,
+                "best_val/dice_score": best_val_dice_score
+            })
 
 
 
@@ -281,8 +449,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
